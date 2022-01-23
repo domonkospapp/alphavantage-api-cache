a19625ec783cee6cfdc717a2b8b4e714b930e783
Domonkos Papp <pappdomonkos96@gmail.com>
2022-01-23 20:59:49 +0100

    Use api key and validate return data

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import express from "express"
 import fetch from "node-fetch"
 import { createClient } from 'redis';
+import dotenv from 'dotenv'
 
+dotenv.config()
 const PORT = process.env.PORT || 3000
 const REDIS_PORT = process.env.REDIS_PORT || 6379
+const ALPHA_VANTAGE_API_KEY = process.env.ALPHA_VANTAGE_API_KEY || "demo"
 
 const redisClient = createClient(REDIS_PORT);
 redisClient.on('error', (err) => console.log('Redis Client Error', err));
@@ -11,17 +14,25 @@ redisClient.on('connect', () => console.log('Connected!'));
 await redisClient.connect();
 
 const cache = async(req, res, next) => {
-    const data = await redisClient.get('IBM');
+    const { companyName } = req.params;
+    const data = await redisClient.get(companyName);
     data ? res.send(data) : next()
 }
 
 const getCompany = async (req, res, next) => {
     try{
-        const response = await fetch(`https://www.alphavantage.co/query?function=OVERVIEW&symbol=IBM&apikey=demo`)
+        const { companyName } = req.params;
+        const response = await fetch(`https://www.alphavantage.co/query?function=OVERVIEW&symbol=${companyName}&apikey=${ALPHA_VANTAGE_API_KEY}`)
         const data = await response.json();
 
-        redisClient.setEx("IBM", 5, JSON.stringify(data));
-        res.send(data);
+        if(data.Symbol === companyName){
+            redisClient.setEx(companyName, 5, JSON.stringify(data));
+            res.send(data);
+        }else{
+            res.send("Wrong ticker!");
+            res.status(500);
+        }
+        
     } catch(err){
         console.error(err);
         res.status(500);
@@ -30,7 +41,7 @@ const getCompany = async (req, res, next) => {
 
 const app = express()
 
-app.get('/', cache, getCompany);
+app.get('/:companyName', cache, getCompany);
 
 app.listen(PORT, ()=>{
     console.log(`App listening on port ${PORT}`)
